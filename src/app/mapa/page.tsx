25e04906bb0ed9e69de28e009591c8cb2a7dc9c1
Marcelo H Davini <mhd_look@hotmail.com>
2024-08-30 18:23:46 +0000

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/components/map.tsx 	new file:   src/app/mapa/layout.tsx 	new file:   src/app/mapa/page.tsx

---
+++ src/app/mapa/page.tsx
@@ -0,0 +1,23 @@
+// src/page.tsx
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+export default async function Page() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../components/map"), {
+        loading: () => <p>Lendo mapa...</p>,
+        ssr: false,
+      }),
+    []
+  );
+
+  return (
+    <>
+      <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
+        <Map posix={[-23.462007, -46.556279]} />
+      </div>
+    </>
+  );
+}
